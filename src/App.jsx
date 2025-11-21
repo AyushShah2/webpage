4497d7bb2de25630c53000bfed203dc857e7cf2e
Ayush Shah <138225387+AyushShah2@users.noreply.github.com>
2025-11-21 02:00:43 +0000

    Using Hashrouter instead of Browserrouter for github pages support

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { BrowserRouter as Router, Routes, Route} from "react-router-dom";
+import { HashRouter as Router, Routes, Route } from "react-router-dom";
 import { WhaleActivityPrompt, SocialMediaHypePrompt } from "./Prompts";
 import LandingPage from "./LandingPage";
 import "./App.css";
